571505e67e532c51ba1261a2c0a8e7bf27c87c3b
Diamilatou-I <diamilatou.diallo@epitech.digital>
2024-09-09 17:28:42 +0200

    ex_04.js

---
+++ ex_04.js
@@ -0,0 +1,16 @@
+let action = "sub"
+let a = 2
+let b = 4
+function calculator () {
+if (action == "add"){
+return  addition()
+}
+else return subtraction() 
+}
+function addition () {
+return a + b
+}
+function subtraction () {
+return a - b
+}
+
