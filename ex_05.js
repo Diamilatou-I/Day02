5857f9196c8dc05e5397f04d53ae031d917c90bb
Diamilatou-I <diamilatou.diallo@epitech.digital>
2024-09-10 11:25:33 +0200

    ex_05.js

---
+++ ex_05.js
@@ -0,0 +1,6 @@
+let arr = ["strawberry", "strawberry", "strawberry",
+"apple", "apple", "lime", "lime", "peach", "pear", "pear"];
+function getBasketContent() {
+  console.log (arr. length + " fruit(s) selected")
+return arr
+}
